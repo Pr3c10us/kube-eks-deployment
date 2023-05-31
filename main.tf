e1dfcf7df453f5f98d9b0cd89f67c79600427d8b
Abdul-Bakri <66844942+Bakri-10@users.noreply.github.com>
2023-05-31 08:05:26 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -233,3 +233,12 @@ resource "helm_release" "nginx_ingress" {
 output "cluster_id" {
   value = module.eks_cluster.cluster_id
 }
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+  value       = module.eks.cluster_endpoint
+}
