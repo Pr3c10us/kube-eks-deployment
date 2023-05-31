5c06a85758f3ae125e00469afe7072d206a259fb
Abdul-Bakri <66844942+Bakri-10@users.noreply.github.com>
2023-05-31 08:07:41 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -235,10 +235,10 @@ output "cluster_id" {
 }
 output "cluster_certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = module.eks.cluster_certificate_authority_data
+  value       = module.eks_cluster.cluster_certificate_authority_data
 }
 
 output "cluster_endpoint" {
   description = "Endpoint for your Kubernetes API server"
-  value       = module.eks.cluster_endpoint
+  value       = module.eks_cluster.cluster_endpoint
 }
